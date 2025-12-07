9c7070d535ea1a67d4384ab201a6d687f20379df
jose-g315 <120354063+jose-g315@users.noreply.github.com>
2025-12-07 03:03:29 -0600

    set up project repo with nessesary tools and format

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// index.js
+import './styles.css';
+import { greeting } from './greeting.js';
+import odinImage from './images/odin.png';
+
+const image = document.createElement('img');
+image.src = odinImage;
+
+document.body.appendChild(image);
+console.log(greeting);
